ebe2d6658ce752bc723e9aec29898b54df359fa9
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-08-09 13:20:49 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const characters = [
+    "A","B","C","D","E","F","G",
+    "H","I","J","K","L","M","N",
+    "O","P","Q","R","S","T","U",
+    "V","W","X","Y","Z","a","b",
+    "c","d","e","f","g","h","i",
+    "j","k","l","m","n","o","p",
+    "q","r","s","t","u","v","w",
+    "x","y","z","0","1","2","3",
+    "4","5","6","7","8","9","~",
+    "`","!","@","#","$","%","^",
+    "&","*","(",")","_","-","+",
+    "=","{","[","}","]",",","|",
+    ":",";","<",">",".","?","/"
+];
+
+let pass1El = document.getElementById("pass1");
+let pass2El = document.getElementById("pass2");
+
+function generatePasswords() {
+    let password1 = "";
+    let password2 = "";
+
+    for(let i = 0; i < 15; i++){
+        password1 += characters[Math.floor(Math.random() * characters.length)];
+        password2 += characters[Math.floor(Math.random() * characters.length)];
+    }
+
+    pass1El.textContent = password1;
+    pass2El.textContent = password2;
+}
